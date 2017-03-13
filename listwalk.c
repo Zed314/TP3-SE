0db48e764ead4754cebba470882f495a1e953c51
Zed314 <ziggy.vergne@insa-lyon.fr>
2017-03-13 20:05:26 +0100

    Fin

--- listwalk.c
+++ listwalk.c
@@ -4,6 +4,28 @@
 #include <time.h>
 #include <string.h>
 #include <assert.h>
+
+// returns a human-friendly string describing this many bytes e.g. 1024 -> "1kB"
+char * hsize(int64_t bytes)
+{
+const char *units[]={"B","kB","MB","GB","TB","PB","EB","ZB","YB"};
+int unit=0;
+// 7 bytes is our worst-case: three significant digits, a decimal point in the middle,
+// the two-letter unit, and then the terminating NUL byte
+char* result = malloc(7);
+assert(result != NULL);
+// the mismatch (1000 vs 1024) here is intentional.
+// it helps avoiding horrors like 1023 -> "1e+03B"
+float number=bytes;
+while( number>1000 )
+{
+number /= 1024;
+unit++;
+}
+snprintf(result,7,"%.3g%s",number,units[unit]);
+return result;
+}
+
 double benchmark(int64_t N,int64_t R)
 {
 	srandom(clock());
@@ -45,32 +67,32 @@ double benchmark(int64_t N,int64_t R)
 int main()
 {
 	
-	double tab[50];
+	double tab[1000];
 	int i=0;
 	int64_t N=1000;
 	int64_t R=10000;
-	for(i=0;i<50;i++)
+	for(i=0;i<1000;i++)
 	{
 		int64_t temps=clock();
 		tab[i]=benchmark( N,R);
-		printf("%ld\r\n",N*8);
+		printf("%s %ld %g\r\n",hsize(N*8),N*8,tab[i]);
 		if((((clock()-temps))/CLOCKS_PER_SEC)>1)
 		{
-			if(R>1)
+			if(R>4)
 			{
 			R/=2;
 			}
 		}
-		N=N*1.25;
+		N=N*1.1;
 		//printf("%g\r\n",tab[i]);
 		
 		
 	}
-		for(i=0;i<50;i++)
+		/*for(i=0;i<50;i++)
 	{
 		printf("%g\r\n",tab[i]);
 		
-	}
+	}*/
 	
 	
     return 0;
